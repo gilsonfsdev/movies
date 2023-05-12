b7f3bb3c7a41c6f67788264bd56527359bb57fd5
Gilson Ferreira <gilsonfsdev@gmail.com>
2023-05-12 14:13:06 -0300

    title

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -16,7 +16,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>Movies</title>
         <meta name="description" content="aplicação que dá acesso a uma database de filmes" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
